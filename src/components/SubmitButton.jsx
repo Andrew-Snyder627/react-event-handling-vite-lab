2f611510ed624b6cf2f3150b60ac5da3708682af
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-06-04 11:46:20 -0400

    Submit button logic and event listening in place

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,22 @@
 // Code SubmitButton Component Here
 
-function SubmitButton (){
-    return(
-        <></>
-    )
+// Render a submit button
+// track mouse enter/leave events
+function SubmitButton() {
+  // Handler for mouse entering the button
+  function handleEnter() {
+    console.log("Mouse Entering");
+  }
+  // Handler for mouse leaving the button
+  function handleLeave() {
+    console.log("Mouse Exiting");
+  }
+  // Render
+  return (
+    <button onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+      Submit Password
+    </button>
+  );
 }
 
 export default SubmitButton;
